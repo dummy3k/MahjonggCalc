02646bf875d7b438fd24f905c4946d8f56b6d255
dummy3k <l4711@gmx.net>
2010-11-28 12:19:32 +0100

    worked on add score layout

--- dummy.MahjonggCalc/src/dummy/MahjonggCalc/AddScoreActivity.java
+++ dummy.MahjonggCalc/src/dummy/MahjonggCalc/AddScoreActivity.java
@@ -2,12 +2,20 @@ package dummy.MahjonggCalc;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class AddScoreActivity extends Activity {
+	private static final String TAG = "AddScoreActivity";
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_score_activity);
+    }
+
+    public void onLabelClick(View view) {
+    	Log.d(TAG, "onLabelClick");
     }
 }
