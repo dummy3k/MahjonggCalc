09a2b2d8959bc6927cd5df4e19d304e6e5092a5d
dummy3k <l4711@gmx.net>
2010-11-28 10:36:05 +0100

    initial commit

---
+++ dummy.MahjonggCalc/src/dummy/MahjonggCalc/AddScoreActivity.java
@@ -0,0 +1,13 @@
+package dummy.MahjonggCalc;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AddScoreActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_score_activity);
+    }
+}
